2bd0ded80745dc520c279c328855c738306daccc
Dmitriy Pleshevskiy <dmitriy@ideascup.me>
2022-12-04 23:45:53 +0300

    add all keywords

--- grammar.js
+++ grammar.js
@@ -7,26 +7,11 @@ module.exports = grammar({
     // TODO: add the actual grammar rules
     source_file: ($) => repeat($._definition),
 
-    _definition: ($) => choice($.attribute, $.connection, $.shape),
-
-    _end: ($) => choice(";", "\n", "\0"),
-
-    attribute: ($) =>
+    _definition: ($) =>
       choice(
-        seq(
-          $.identifier,
-          repeat1(seq($.dot, $.keyword)),
-          ":",
-          alias($.label, $.attribute_value),
-          $._end
-        ),
-        seq(
-          $.keyword,
-          repeat(seq($.dot, $.keyword)),
-          ":",
-          alias($.label, $.attribute_value),
-          $._end
-        )
+        choice($._root_attribute, $._shape_attribute),
+        $.connection,
+        $.shape
       ),
 
     connection: ($) =>
@@ -47,13 +32,73 @@ module.exports = grammar({
         $._end
       ),
 
-    dot: ($) => ".",
-
     label: ($) => choice($.string, $._unquoted_string),
 
     identifier: ($) => $._identifier,
 
-    keyword: ($) => choice("direction"),
+    attr_value: ($) => choice($.string, $._unquoted_string),
+
+    _root_attribute: ($) =>
+      seq(
+        alias($._root_attr_key, $.attr_key),
+        ":",
+        alias($.label, $.attr_value),
+        $._end
+      ),
+
+    _root_attr_key: ($) => "direction",
+
+    _style_attribute: ($) =>
+      seq(
+        alias("style", $.attr_key),
+        $.dot,
+        alias($._style_attr_key, $.attr_key),
+        ":",
+        $.attr_value,
+        $._end
+      ),
+
+    _shape_attribute: ($) =>
+      seq(
+        $.identifier,
+        $.dot,
+        choice(
+          seq(alias($._shape_attr_key, $.attr_key), ":", $.attr_value, $._end),
+          $._style_attribute
+        )
+      ),
+
+    _shape_attr_key: ($) =>
+      choice(
+        "shape",
+        "label",
+        // sql
+        "constraint",
+        // image
+        "icon",
+        "width",
+        "height"
+      ),
+
+    _style_attr_key: ($) =>
+      choice(
+        "opacity",
+        "fill",
+        "stroke",
+        "stroke-width",
+        "stroke-dash",
+        "border-radius",
+        "font-color",
+        "shadow",
+        "multiple",
+        "animated",
+        "3d",
+        "link"
+      ),
+
+    _text_attr_key: ($) => "near",
+
+    _connection_attr_key: ($) => choice("source-arrowhead", "target-arrowhead"),
 
     _identifier: ($) =>
       prec.right(
@@ -91,5 +136,9 @@ module.exports = grammar({
         seq('"', repeat(token(/[^'\n]+/)), '"'),
         seq("`", repeat(token(/[^'\n]+/)), "`")
       ),
+
+    _end: ($) => choice(";", "\n", "\0"),
+
+    dot: ($) => ".",
   },
 });
